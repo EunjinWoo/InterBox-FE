6d6780348b7350a1078a9a99caf5922596c27074
EunjinWoo <esther0904@naver.com>
2025-02-25 18:16:32 +0900

    [Feat] 메인 페이지 상단 바 UI 구현

---
+++ lib/widgets/questionsList.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QuestionsList extends StatelessWidget {
+  const QuestionsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
