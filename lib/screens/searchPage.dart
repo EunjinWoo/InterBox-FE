967a027f6da4f66da94e7cc38e7f97f67bae173d
EunjinWoo <esther0904@naver.com>
2025-02-25 22:37:56 +0900

    [Feat] mock json data 연결 및 질문 리스트 UI 구현

--- lib/screens/searchPage.dart
+++ lib/screens/searchPage.dart
@@ -1,9 +1,38 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:inter_box/widgets/searchPageNotice.dart';
 import 'package:inter_box/widgets/searchPageSearchBar.dart';
 
-class SearchPage extends StatelessWidget {
+import '../widgets/questionsList.dart';
+
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  String closestJob = '';
+  List<Map<String, dynamic>> questions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadQuestions();
+  }
+
+  // JSON 파일에서 질문 데이터 불러오기
+  Future<void> loadQuestions() async {
+    final String response = await rootBundle.loadString('assets/data/mock_search_api.json');
+    final data = json.decode(response);
+    setState(() {
+      closestJob = data['closest_job'];
+      questions = List<Map<String, dynamic>>.from(data['questions']);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +45,10 @@ class SearchPage extends StatelessWidget {
       body: Column(
         children: [
           SearchPageSearchBar(),
-          SearchPageNotice(closestJob: "웹 프론트엔드"),
+          SearchPageNotice(closestJob: closestJob),
+          Expanded(
+            child: QuestionsList(questions: questions),
+          ),
         ],
       ),
     );
