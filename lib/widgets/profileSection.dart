c7bb85ed5b68337f3dbf9100cc52eddb279b5031
EunjinWoo <esther0904@naver.com>
2025-02-26 02:39:47 +0900

    [Feat] 마이페이지 UI 구조 구성

---
+++ lib/widgets/profileSection.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProfileSection extends StatelessWidget {
+  final Map<String, dynamic> profile;
+
+  const ProfileSection({super.key, required this.profile});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
