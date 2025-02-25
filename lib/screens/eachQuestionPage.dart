910d4d7a2096fd0b301427ac4a42ae06b74698d4
EunjinWoo <esther0904@naver.com>
2025-02-26 01:00:09 +0900

    [Feat] 각 질문 클릭 시 각 질문 페이지로 이동하도록 설정

---
+++ lib/screens/eachQuestionPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EachQuestionPage extends StatelessWidget {
+  const EachQuestionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
--- lib/widgets/eachQuestion.dart
+++ lib/widgets/eachQuestion.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:inter_box/screens/eachQuestionPage.dart';
 
 class EachQuestion extends StatelessWidget {
   final Map<String, dynamic> question;
@@ -7,40 +8,48 @@ class EachQuestion extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.symmetric(vertical: 8),
-        padding: EdgeInsets.symmetric(horizontal: 18, vertical: 16),
-        decoration: BoxDecoration(
-          color: Color(0xFFF3F3F3),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        constraints: BoxConstraints(
-          minHeight: 100, // 최소 높이 100px
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              question['content'],
-              style: TextStyle(
-                fontSize: 16,
-                fontFamily: 'Pretendard-SemiBold',
-                color: Color(0xFF3B463C),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => EachQuestionPage()),
+        );
+      },
+      child: Container(
+          margin: EdgeInsets.symmetric(vertical: 8),
+          padding: EdgeInsets.symmetric(horizontal: 18, vertical: 16),
+          decoration: BoxDecoration(
+            color: Color(0xFFF3F3F3),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          constraints: BoxConstraints(
+            minHeight: 100, // 최소 높이 100px
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                question['content'],
+                style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: 'Pretendard-SemiBold',
+                  color: Color(0xFF3B463C),
+                ),
               ),
-            ),
-            Row(
-              children: [
-                Spacer(),
-                Image.asset(
-                  question['is_solved'] ? 'assets/images/solved.png' : 'assets/images/unsolved.png',
-                  width: 28,
-                  height: 28,
-                )
-              ],
-            ),
-          ],
-        )
+              Row(
+                children: [
+                  Spacer(),
+                  Image.asset(
+                    question['is_solved'] ? 'assets/images/solved.png' : 'assets/images/unsolved.png',
+                    width: 28,
+                    height: 28,
+                  )
+                ],
+              ),
+            ],
+          )
+      ),
     );
   }
 }
