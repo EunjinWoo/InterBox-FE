60573e87ecd91d350892bf0c5b0c46cc1034809e
EunjinWoo <esther0904@naver.com>
2025-02-25 22:32:32 +0900

    [Fix] questionsList 내부 Expanded 삭제

--- lib/widgets/questionsList.dart
+++ lib/widgets/questionsList.dart
@@ -7,52 +7,50 @@ class QuestionsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        color: Colors.white,
-        child: questions.isEmpty
-            ? Center(child: CircularProgressIndicator()) // 로딩 중 표시
-            : ListView.builder(
-          padding: EdgeInsets.symmetric(vertical: 0, horizontal: 16),
-          itemCount: questions.length,
-          itemBuilder: (context, index) {
-            return Container(
-                margin: EdgeInsets.symmetric(vertical: 8),
-                padding: EdgeInsets.symmetric(horizontal: 18, vertical: 16),
-                decoration: BoxDecoration(
-                  color: Color(0xFFF3F3F3),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                constraints: BoxConstraints(
-                  minHeight: 100, // 최소 높이 100px
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      questions[index]['content'],
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontFamily: 'Pretendard-SemiBold',
-                        color: Color(0xFF3B463C),
-                      ),
+    return Container(
+      color: Colors.white,
+      child: questions.isEmpty
+          ? Center(child: CircularProgressIndicator()) // 로딩 중 표시
+          : ListView.builder(
+        padding: EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+        itemCount: questions.length,
+        itemBuilder: (context, index) {
+          return Container(
+              margin: EdgeInsets.symmetric(vertical: 8),
+              padding: EdgeInsets.symmetric(horizontal: 18, vertical: 16),
+              decoration: BoxDecoration(
+                color: Color(0xFFF3F3F3),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              constraints: BoxConstraints(
+                minHeight: 100, // 최소 높이 100px
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    questions[index]['content'],
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Pretendard-SemiBold',
+                      color: Color(0xFF3B463C),
                     ),
-                    Row(
-                      children: [
-                        Spacer(),
-                        Image.asset(
-                          questions[index]['is_solved'] ? 'assets/images/solved.png' : 'assets/images/unsolved.png',
-                          width: 28,
-                          height: 28,
-                        )
-                      ],
-                    ),
-                  ],
-                )
-            );
-          },
-        ),
+                  ),
+                  Row(
+                    children: [
+                      Spacer(),
+                      Image.asset(
+                        questions[index]['is_solved'] ? 'assets/images/solved.png' : 'assets/images/unsolved.png',
+                        width: 28,
+                        height: 28,
+                      )
+                    ],
+                  ),
+                ],
+              )
+          );
+        },
       ),
     );;
   }
